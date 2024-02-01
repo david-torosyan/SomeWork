1ba17f7f93ea4dedf1a8571a55603a34fc768599
Torosyan David <davit.torosyan.0214@gmail.com>
2024-02-01 17:41:28 +0400

    Add project files.

---
+++ SomeWork.Tests/CalculatorTest.cs
@@ -0,0 +1,41 @@
+﻿using UnitTestExample;
+using Xunit;
+
+namespace SomeWork.Tests
+{
+    public class CalculatorTest
+    {
+        [Fact]
+        public void Add_ShouldCalculate()
+        {
+            // Expected
+            double expected = 13;
+
+            // Actual
+            double actual = Calculator.Add(5, 8);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        /// <summary>
+        /// It takes arguments.
+        /// </summary>
+        /// <param name="x">arg1.</param>
+        /// <param name="y">arg2.</param>
+        /// <param name="expected">expected.</param>
+        [Theory]
+        [InlineData(5, 4, 9)]
+        [InlineData(5.25, 12, 17.25)]
+        public void Add_ShouldCalculate2(double x, double y, double expected)
+        {
+            // Expected
+
+            // Actual
+            double actual = Calculator.Add(x, y);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ SomeWork/Calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestExample
+{
+    public static class Calculator
+    {
+        public static double Add(double val1, double val2)
+        {
+            return val1 + val2;
+        }
+        public static double Multiple(double val1, double val2)
+        {
+            return val1 * val2;
+        }
+        public static double Divide(double val1, double val2)
+        {
+            if ((val2 == 0))
+            {
+                throw new DivideByZeroException();
+            }
+            return val1 / val2;
+        }
+    }
+}
